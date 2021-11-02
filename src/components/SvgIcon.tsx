b23fbc69c83fdd8e3e8204f38bfc5e296886f2e5
Eugene Blokhin <mayson2@yandex.ru>
2021-11-02 07:06:15 +0300

    feat: introduce a separate component for svg icons

---
+++ src/components/SvgIcon.tsx
@@ -0,0 +1,7 @@
+// TODO: consider moving to spritesheet
+import { ReactComponent as Humidity } from 'assets/svg-icons/Humidity.svg'
+import { ReactComponent as Pressure } from 'assets/svg-icons/Pressure.svg'
+import { ReactComponent as Wind } from 'assets/svg-icons/Wind.svg'
+import { ReactComponent as Add } from 'assets/svg-icons/Add.svg'
+
+export { Humidity, Pressure, Wind, Add }
