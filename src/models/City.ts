63723c199cc0072e4b2b60f7f213e3ab10e7b039
Eugene Blokhin <mayson2@yandex.ru>
2021-11-02 07:24:49 +0300

    feat(ts): add city interfaces

--- src/models/City.ts
+++ src/models/City.ts
@@ -1,12 +1,32 @@
-type TCity = {
-  name: string
-  initialized: boolean
+import { runInAction, makeAutoObservable } from 'mobx'
+
+export interface IWeatherData {
   temp: number
   humidity: number
   wind: number
   pressure: number
-  description: string
-  weatherIcon: string
+  weatherDescription: string
+  weatherIcons: string[]
 }
 
-export default TCity
+export interface ICity extends IWeatherData {
+  id?: number
+  name: string
+  countryID: string
+  latLong: [lat: number, lon: number]
+  initialized: boolean
+}
+
+export type TCityInfo = Pick<ICity, 'name' | 'countryID' | 'latLong'>
+
+class CityAutocomplete {
+  items: TCityInfo[] = []
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  async getItems() {
+    
+  }
+}
