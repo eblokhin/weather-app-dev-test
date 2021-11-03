70f8ecd3c0b35e86afc25bba97ffe3921d62e1c6
Eugene Blokhin <mayson2@yandex.ru>
2021-11-03 23:21:04 +0300

    feat: add useWindowEvent hook

---
+++ src/hooks/useWindowEvent.ts
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+
+const useWindowEvent = (eventType: keyof WindowEventMap, handler: (e: Event) => void): void => {
+  useEffect(() => {
+    window.addEventListener(eventType, handler)
+
+    return () => {
+      window.removeEventListener(eventType, handler)
+    }
+  }, [])
+}
+
+export default useWindowEvent
