dc916e8b57542dcc3eb98bc99804f3d3aab9f8e8
Eugene Blokhin <mayson2@yandex.ru>
2021-11-03 23:19:26 +0300

    feat: update city items to render different states

--- src/components/CityItem.tsx
+++ src/components/CityItem.tsx
@@ -1,48 +1,58 @@
 import type { FunctionComponent } from 'react'
-import type { ICity } from 'models/City'
+import type City from 'store/city'
 
 import cn from 'classnames'
 
 import WeatherIcon from 'components/WeatherIcon'
 import Measurement from 'components/Measurement'
 import { MeasurementType } from 'models/MeasurementType'
+import Spinner from 'components/Spinner'
 
 import styles from './CityItem.module.sass'
+import { observer } from 'mobx-react-lite'
+import { citiesCollection } from 'store'
 
 interface IProps {
-  city: ICity
+  city: City
   className?: string
 }
 
 const CityItem: FunctionComponent<IProps> = ({ city, className }) => {
-  const { name, pressure, weatherIcons: weatherIcon, temp, humidity, wind, weatherDescription, initialized } = city
+  const { name, initialized, pending, weatherData } = city
+
+  const removeItem = citiesCollection.removeItem
 
   return (
     <section className={cn(styles.block, className)}>
       <div className={styles.infoSection}>
         <h2 className={styles.title}>{name}</h2>
-        {initialized && (
+        {initialized && weatherData && (
           <>
             <div className="flex items-center mb-1">
-              <div className="text-5xl mr-4">{temp}&deg;C</div>
-              {weatherIcon.map((icon) => (
+              <div className="text-5xl mr-4">{weatherData.temp}&deg;C</div>
+              {weatherData.icons.map((icon) => (
                 <WeatherIcon key={icon} icon={icon} className={styles.weatherStatusIcon} />
               ))}
             </div>
-            <div className="text-sm text-gray-400 mb-2 capitalize">{weatherDescription}</div>
+            <div className="text-sm text-gray-400 mb-2 capitalize">{weatherData.description}</div>
           </>
+        )}
+        {!initialized && pending && (
+          <div className="h-16 relative">
+            <Spinner className={styles.spinner} />
+          </div>
         )}
       </div>
       <div className={cn(styles.infoSection, styles.measurements)}>
-        <Measurement className={styles.measurementItem} type={MeasurementType.Wind} value={wind} />
-        <Measurement className={styles.measurementItem} type={MeasurementType.Humidity} value={humidity} />
-        <Measurement className={styles.measurementItem} type={MeasurementType.Pressure} value={pressure} />
+        <Measurement className={styles.measurementItem} type={MeasurementType.Wind} value={weatherData?.wind} />
+        <Measurement className={styles.measurementItem} type={MeasurementType.Humidity} value={weatherData?.humidity} />
+        <Measurement className={styles.measurementItem} type={MeasurementType.Pressure} value={weatherData?.pressure} />
       </div>
-      <button className={styles.removeButton} title="Remove city">
-        <span className="sr-only">Remove city</span>
+      <button className={styles.removeButton} title={`Remove ${city.name}`} onClick={() => removeItem(city)}>
+        <span className="sr-only">Remove {city.name}</span>
       </button>
     </section>
   )
 }
 
-export default CityItem
+export default observer(CityItem)
--- src/components/CityList.tsx
+++ src/components/CityList.tsx
@@ -1,17 +1,20 @@
 import type { VFC } from 'react'
-import type { ICity } from 'models/City'
+import type CitiesCollection from 'store/citiesCollection'
 
 import cn from 'classnames'
 
 import CityItem from './CityItem'
+import { observer } from 'mobx-react-lite'
 
 interface IProps {
-  items: ICity[]
+  itemsStore: CitiesCollection
   className?: string
 }
 
-const CityList: VFC<IProps> = ({ items, className }) => {
-  if (!items.length) {
+const CityList: VFC<IProps> = ({ itemsStore, className }) => {
+  const { items } = itemsStore
+
+  if (!items.size) {
     return (
       <div className="col-span-full h-40 flex items-center text-center justify-center text-2xl">
         Pick some city using search to track weather in it
@@ -21,11 +24,11 @@ const CityList: VFC<IProps> = ({ items, className }) => {
 
   return (
     <>
-      {items.map((city) => (
-        <CityItem className={cn(className)} city={city} key={`${city.name}, ${city.countryID}`} />
+      {Array.from(items.values(), (city) => (
+        <CityItem className={cn(className)} city={city} key={`${city.key}`} />
       ))}
     </>
   )
 }
 
-export default CityList
+export default observer(CityList)
