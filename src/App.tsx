5acdc954c9d6730ca07e7a3e4e19353be6b3c529
Eugene Blokhin <mayson2@yandex.ru>
2021-11-03 23:28:13 +0300

    feat: enable stores in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,19 @@ import SearchInput from 'components/SearchInput'
 import CityList from 'components/CityList'
 
 import styles from './App.module.sass'
-
-import city from 'fixtures/city'
+import { cityAutocomplete, citiesCollection } from 'store'
 
 const App: FunctionComponent = () => {
   return (
-    <div className="container max-w-5xl mx-auto">
+    <div className="container max-w-5xl mx-auto pb-20 px-5">
       <Header />
       <div className={styles.layout}>
         <div className={styles.intro}>
           <h1>Weather forecast</h1>
           <p>Simple but powerful weather forcasting service based on OpenWeatherMap API</p>
         </div>
-        <SearchInput className={styles.search} />
-        <CityList items={[city, city]} />
+        <SearchInput className={styles.search} store={cityAutocomplete} addItem={citiesCollection.addItem} />
+        <CityList itemsStore={citiesCollection} />
       </div>
     </div>
   )
